a3b3a81258628a62a5e25b2f8bb7b10f1f3b89eb
HugoNicolau <nicolau.hugogiles@gmail.com>
2025-04-06 21:46:37 -0300

    Override toString method in Pessoa class to include name, CPF, and telephone

--- Pessoa.java
+++ Pessoa.java
@@ -8,25 +8,37 @@ public abstract class Pessoa {
         this.cpf = cpf;
         this.telefone = telefone;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
     public String getTelefone() {
         return telefone;
     }
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
     // Metodo abstrato para ser implementado nas subclasses
     public abstract void exibirInformacoes();
+
+    @Override
+    public String toString() {
+        return nome + " | CPF: " + cpf + " | Tel: " + telefone;
+    }
+
 }
