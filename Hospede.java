2ba3486e22c888c763069f947d3972da1ea70250
HugoNicolau <nicolau.hugogiles@gmail.com>
2025-04-06 21:44:22 -0300

    Override toString method in Hospede class to include name and email

--- Hospede.java
+++ Hospede.java
@@ -22,5 +22,10 @@ public class Hospede extends Pessoa {
         System.out.println("Email: " + getEmail());
         System.out.println("------------------------------");
     }
+
+    @Override
+    public String toString() {
+        return getNome() + " - " + getEmail();
+    }
     
 }
